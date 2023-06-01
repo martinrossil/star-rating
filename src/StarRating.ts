79fdbb018d2d2c3203e0a18ff43fa7ac623dc085
Martin Rossil <martinrossil@gmail.com>
2023-06-01 21:52:53 +0200

    Implemented IStarRating interface.

--- src/StarRating.ts
+++ src/StarRating.ts
@@ -1,20 +1,48 @@
-export default class StarRating extends HTMLElement {
-	public static get observedAttributes() {
-		return ['disabled', 'value'];
-	}
+import IStarRating from './IStarRating';
 
+export default class StarRating extends HTMLElement implements IStarRating {
 	public constructor() {
 		super();
-		this.style.background = 'red';
-		this.style.display = 'inline-flex';
-		this.style.minWidth = '250px';
-		this.style.minHeight = '16px';
 	}
 
-	public attributeChangedCallback(name: string, oldValue: string | null, newValue: string) {
-		console.log('name [' + name + ']', 'old [' + oldValue + ']', 'new [' + newValue + ']');
-		console.log(oldValue === null);
-		console.log(newValue === '');
+	private _value = 0;
+
+	public get value() {
+		return this._value;
+	}
+
+	public set value(value: number) {
+		this._value = value;
+	}
+
+	private _disabled = false;
+
+	public get disabled() {
+		return this._disabled;
+	}
+
+	public set disabled(value: boolean) {
+		this._disabled = value;
+	}
+
+	private _size: 'small' | 'medium' | 'large' = 'medium';
+
+	public get size() {
+		return this._size;
+	}
+
+	public set size(value: 'small' | 'medium' | 'large') {
+		this._size = value;
+	}
+
+	private _readOnly = false;
+
+	public get readOnly() {
+		return this._readOnly;
+	}
+
+	public set readOnly(value: boolean) {
+		this._readOnly = value;
 	}
 }
 customElements.define('star-rating', StarRating);
