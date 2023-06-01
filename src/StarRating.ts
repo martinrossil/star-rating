1bd67fbbf1bfe8a10a235f98375ec6a08001d8e9
Martin Rossil <martinrossil@gmail.com>
2023-06-01 16:18:25 +0200

    Created StarRating custom element..

--- src/StarRating.ts
+++ src/StarRating.ts
@@ -1,6 +1,20 @@
 export default class StarRating extends HTMLElement {
+	public static get observedAttributes() {
+		return ['disabled', 'value'];
+	}
+
 	public constructor() {
 		super();
+		this.style.background = 'red';
+		this.style.display = 'inline-flex';
+		this.style.minWidth = '250px';
+		this.style.minHeight = '16px';
+	}
+
+	public attributeChangedCallback(name: string, oldValue: string | null, newValue: string) {
+		console.log('name [' + name + ']', 'old [' + oldValue + ']', 'new [' + newValue + ']');
+		console.log(oldValue === null);
+		console.log(newValue === '');
 	}
 }
 customElements.define('star-rating', StarRating);
